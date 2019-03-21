8b825dc444e63d4a476daff3246d58197b794c91
CodeXRook <lacekitfly@gmail.com>
2019-03-21 03:15:16 -0400

    UPDATED test comments

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,5 @@
 test('it should pass', () => {
     expect(true).toBe(true);
 });
+//WANT TO MAKE TEST RUN TO UNBLOCK DEPLOYMENT WE WANT TO DEPLOY NOT ACTUALLY TEST
+//EG. COULD HAVE BEEN CALL FOOBAR
