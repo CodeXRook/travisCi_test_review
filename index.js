31b801e6854dbf135dbd1c201f965fd564823364
CodeXRook <lacekitfly@gmail.com>
2019-03-21 02:08:01 -0400

    add heroku port process

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/ping', (req, res) => {
     res.json({'pong': '🏓'});
 });
 
-const port = 5020;
+const port = process.env.PORT || 5020;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
