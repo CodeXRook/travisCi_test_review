8393c91e4644f01e8c43fdc6861de210992baf48
CodeXRook <lacekitfly@gmail.com>
2019-03-21 00:40:59 -0400

    what did i do?

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ const port = 5001;
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
 })
+
+//WHAT DID I DO?
+//CREATED EXPRESS  
+//CREATE EXPRESS APP
+//ADD ROUTE ADDED GET ROUTE CALLED PING WHICH WILL RESPONDE TO PONG AND ADD A PORT ROUTE
