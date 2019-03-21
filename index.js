24795186a22e5f7c95cce66f6875fcdd48200333
CodeXRook <lacekitfly@gmail.com>
2019-03-21 00:28:59 -0400

    ping require & response

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require('express')
 //npm install --save express/ express required for package json
 const app = express();
 //CREATING A SIMPLE GET REQUEST 
+
+app.get('/ping',(req, res)=>{
+    
+})
